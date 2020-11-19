bb70d71f674d2019f91a250f272c786320b0be58
Steven Morrison <smorrison.dev@gmail.com>
2020-11-19 22:54:56 +0000

    SM: typescript added

---
+++ dist/modules/challenge-1/resolvers.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+module.exports = {
+    Query: {
+        getUserByEmail: (obj, args, context) => {
+            console.log("test");
+            return {
+                userId: args.email,
+                email: "john@test.com",
+                forename: "john",
+                surname: "doe"
+            };
+        }
+    },
+    Mutation: {
+        updateProfile: (obj, args, context) => {
+            return {
+                userId: args.id,
+                email: "update email",
+                forename: "test",
+                surname: "testing"
+            };
+        }
+    }
+};
+//# sourceMappingURL=resolvers.js.map
---
+++ dist/modules/challenge-2/resolvers.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+module.exports = {
+    Query: {
+        getUsersBySurname: (obj, args, context) => {
+            return [
+                {
+                    userId: args.surname,
+                    email: "jane@test.com",
+                    forename: "jane",
+                    surname: "doe"
+                },
+                {
+                    userId: args.surname,
+                    email: "john@test.com",
+                    forename: "john",
+                    surname: "doe"
+                }
+            ];
+        }
+    },
+    User: {
+        teams: (obj, args, context) => {
+            return [
+                {
+                    teamId: 1,
+                    name: "team 1"
+                },
+                {
+                    teamId: 2,
+                    name: "team 2"
+                }
+            ];
+        }
+    }
+};
+//# sourceMappingURL=resolvers.js.map
