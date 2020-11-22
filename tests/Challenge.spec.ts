aed80318e4dc2843fff42ef6485f99a6a42f9588
Steven Morrison <smorrison.dev@gmail.com>
2020-11-22 17:57:46 +0000

    SM: first test setup

---
+++ tests/Challenge.spec.ts
@@ -0,0 +1,20 @@
+import * as mocha from "mocha";
+import * as chai from "chai";
+
+import Challenge from "./../src/core/Challenge";
+
+describe("Challenge.ts", () => {
+
+    describe("When challenge is declared and enabled", () => {
+
+        xit("challenge configuration should be setup", () => {
+            const challenge = new Challenge("challenge-test", "test challenge", "10", true);
+        });
+
+        xit("set path to module folder depending on difficulty", () => {
+
+        });
+
+    });
+
+});
