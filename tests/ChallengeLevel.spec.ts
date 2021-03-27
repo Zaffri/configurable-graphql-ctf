1ec7ebda6846e3589b065d6bec84905ed89c14ba
Steven Morrison <smorrison.dev@gmail.com>
2021-03-27 18:31:53 +0000

    SM: challenge level tests fixed

--- tests/ChallengeLevel.spec.ts
+++ tests/ChallengeLevel.spec.ts
@@ -1,17 +1,40 @@
-import * as path from "path";
 import { expect } from "chai";
 import dotenv from "dotenv";
+import Challenge from "../src/core/Challenge";
+import ChallengeLevel from "../src/core/ChallengeLevel";
 dotenv.config();
-
-import ChallengeLvel from "./../src/core/ChallengeLevel";
 
 process.env.BASE_PROJECT_PATH = __dirname.replace(/tests(\/|\\|)/gi, "");
 
 describe("ChallengeLevel.ts", () => {
     describe("When challenge level extends context", () => {
-        xit("modify GraphQL context", () => { console.log("pending"); });
+        it("modify GraphQL context", () => {
+            const testConfig = {
+                "name": "challenge-3",
+                "description": "Test Module 3",
+                "level": "1",
+                "vulnerable": true,
+                "flag": "FLAG_Rteh42NXir75bvOl"
+            };
+            const challenge = new Challenge(testConfig.name, testConfig.description, testConfig.level, testConfig.vulnerable);
+            const challengeLevel = new ChallengeLevel(challenge);
+
+            expect(challengeLevel.getExtendsContext()).to.equal(true);
+        });
     });
     describe("When challenge level does not extend context", () => {
-        xit("do not modify GraphQL context", () => { console.log("pending"); });
+        it("do not modify GraphQL context", () => {
+            const testConfig = {
+                "name": "challenge-2",
+                "description": "Test Module 2",
+                "level": "2",
+                "vulnerable": true,
+                "flag": "FLAG_xiwrLV9Xirij7vlP"
+            };
+            const challenge = new Challenge(testConfig.name, testConfig.description, testConfig.level, testConfig.vulnerable);
+            const challengeLevel = new ChallengeLevel(challenge);
+
+            expect(challengeLevel.getExtendsContext()).to.equal(false);
+        });
     });
 });
